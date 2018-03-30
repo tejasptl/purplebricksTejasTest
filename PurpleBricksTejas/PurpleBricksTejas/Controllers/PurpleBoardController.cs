c804897746643e992cf77c15eb6621925ca8a3d4
tejasptl <tejasptl89@gmail.com>
2018-03-30 17:13:59 +1100

    Added new page for board cost estimation - WIP

---
+++ PurpleBricksTejas/PurpleBricksTejas/Controllers/PurpleBoardController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PurpleBricksTejas.Models;
+
+namespace PurpleBricksTejas.Controllers
+{
+    public class PurpleBoardController : Controller
+    {
+        //
+        // GET: /PurpleBoard/       
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [AllowAnonymous]
+        public ActionResult BoardPriceEstimator()
+        {
+            PurpleBoardLeaseModel model = new PurpleBoardLeaseModel();            
+
+            return View(model);
+        }
+
+        [HttpPost]
+        [AllowAnonymous]
+        public ActionResult BoardPriceEstimator(PurpleBoardLeaseModel model)
+        {                    
+            return View(model);
+        }
+
+        [AllowAnonymous]
+        public JsonResult GetBoardPrice(string boardSize, string propertyState, string fromDate, string toDate)
+        {
+            PurpleBoardLeaseModel model = new PurpleBoardLeaseModel();
+
+            model.BoardSize = boardSize;
+            model.Property.State = propertyState;
+            model.FromDate = Convert.ToDateTime(fromDate);
+            model.ToDate = Convert.ToDateTime(toDate);
+
+            model.CalculateBoardPrice();
+           
+            return Json("{\"Cost\": " + model.Cost + "}", "application/json", JsonRequestBehavior.AllowGet);           
+        }
+
+
+    }
+}
